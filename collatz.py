4e67d0023f0f69f9e3baddd5efb05d5408b1b56e
Fred Frey <f.frey6@gmail.com>
2015-07-10 19:01:56 -0400

    Added -o

--- collatz.py
+++ collatz.py
@@ -5,15 +5,19 @@ import argparse
 VERSION = 0.2
 
 parser = argparse.ArgumentParser(description="Collatz Sequence Calculator", prog="CSG")
-parser.add_argument("-m", "--mode", action="store", type=str, dest="mode", help="Set mode to 'display' for slow output, 'fast' for quick calculation")
+parser.add_argument("-m", "--mode", action="store", type=str, dest="mode", help="Set MODE to 'display' for slow output, 'fast' for quick calculation")
 parser.add_argument("-n", "--number", action="store", type=int, dest="input_n", help="Calculate sequence for 'INPUT_N'")
 parser.add_argument("-v", "--version", action="store_true", dest="version", help="Display version number.")
+parser.add_argument("-o", "--output", action="store", type=str, dest="file_out", help="Output sequences to FILE. Can't be used with 'MODE=\"fast\"'")
 
 args = parser.parse_args()
 
 if args.version:
 	print("CSG (Collatz Sequence Generator), v" + str(VERSION))
 	exit()
+
+if args.file_out:
+	output_file = open(args.file_out, "a")
 
 def collatz(n):
 	start_n = n
@@ -35,11 +39,17 @@ if args.input_n:
 	print(str(args.input_n) + ": " + str(collatz(args.input_n)))
 	exit()
 
+if args.input_n and args.mode == "fast":
+	print("Error. You can either go fast or log. Not both.")
+
 n = 1
 
 while(True):
 	if args.mode != "fast":
-		print(str(n) + ": " + str(collatz(n)))
+		n_string = str(n) + ": " + str(collatz(n))
+		print(n_string)
+		if args.file_out:
+			output_file.write(n_string)
 	if args.mode == "display":
 		time.sleep(2)
 	n = n + 1
