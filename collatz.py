a4606c88e79a9e34a6d383bad0f3f06b5c93fca8
Fred Frey <f.frey6@gmail.com>
2015-07-10 19:24:15 -0400

    Added --start-from and -c.

--- collatz.py
+++ collatz.py
@@ -5,11 +5,12 @@ import argparse
 VERSION = 0.2
 
 parser = argparse.ArgumentParser(description="Collatz Sequence Calculator", prog="CSG")
-parser.add_argument("-m", "--mode", action="store", type=str, dest="mode", help="Set MODE to 'display' for slow output, 'fast' for quick calculation")
-parser.add_argument("-n", "--number", action="store", type=int, dest="input_n", help="Calculate sequence for 'INPUT_N'")
+parser.add_argument("-m", "--mode", action="store", type=str, dest="mode", help="Set MODE to 'display' for slow output, 'fast' for quick calculation.")
+parser.add_argument("-n", "--number", action="store", type=int, dest="input_n", help="Calculate sequence for 'INPUT_N'.")
 parser.add_argument("-v", "--version", action="store_true", dest="version", help="Display version number.")
-parser.add_argument("-o", "--output", action="store", type=str, dest="file_out", help="Output sequences to FILE. Can't be used with 'MODE=\"fast\"'")
-
+parser.add_argument("-o", "--output", action="store", type=str, dest="file_out", help="Output sequences to FILE. Can't be used with 'MODE=\"fast\"'.")
+parser.add_argument("-s", "--start-from", action="store", type=int, dest="start_number", help="Start sequencer from START_NUMBER.")
+parser.add_argument("-c", "--count-steps", action="store_true", dest="count_steps", help="Print out number of steps for sequence.")
 args = parser.parse_args()
 
 if args.version:
@@ -42,11 +43,17 @@ if args.input_n:
 if args.input_n and args.mode == "fast":
 	print("Error. You can either go fast or log. Not both.")
 
-n = 1
+if args.start_number:
+	n = args.start_number
+else:
+	n = 1
 
 while(True):
 	if args.mode != "fast":
-		n_string = str(n) + ": " + str(collatz(n))
+		cn = collatz(n)
+		n_string = str(n) + ": " + str(cn)
+		if args.count_steps:
+			n_string += " " + str(len(cn)) + " steps."
 		print(n_string)
 		if args.file_out:
 			output_file.write(n_string)
